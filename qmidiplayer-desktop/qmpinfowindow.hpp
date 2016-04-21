7457599cb1bbf1d28b4b29bc12d07801b37ad410
Chris Xiong <chirs241097@gmail.com>
2016-04-21 22:11:04 +0800

    Minor UI changes.

--- qmidiplayer-desktop/qmpinfowindow.hpp
+++ qmidiplayer-desktop/qmpinfowindow.hpp
@@ -2,10 +2,28 @@
 #define QMPINFOWINDOW_HPP
 
 #include <QDialog>
+#include <QLabel>
+#include <QMouseEvent>
+#include <QApplication>
+#include <QClipboard>
 
 namespace Ui {
 	class qmpInfoWindow;
 }
+
+class QClickableLabel : public QLabel
+{
+	Q_OBJECT
+	public:
+		explicit QClickableLabel(QWidget *parent=0):QLabel(parent){}
+	protected:
+		void mousePressEvent(QMouseEvent *e)
+		{
+			QLabel::mousePressEvent(e);
+			if(e->buttons()&Qt::LeftButton)
+				QApplication::clipboard()->setText(text());
+		}
+};
 
 class qmpInfoWindow : public QDialog
 {
